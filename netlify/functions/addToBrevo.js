efaf141726fbae3d514e70f91a6a69328e075516
Forge-site1 <work.temoe@gmail.com>
2026-06-10 21:42:37 +0200

    Update addToBrevo.js

--- netlify/functions/addToBrevo.js
+++ netlify/functions/addToBrevo.js
@@ -1,1 +1,37 @@
+exports.handler = async (event) => {
+  try {
+    const { email } = JSON.parse(event.body);
 
+    const response = await fetch(
+      'https://api.brevo.com/v3/contacts',
+      {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'api-key': process.env.BREVO_API_KEY
+        },
+        body: JSON.stringify({
+          email,
+          listIds: [4],
+          updateEnabled: true
+        })
+      }
+    );
+
+    const data = await response.text();
+
+    return {
+      statusCode: response.status,
+      body: data
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
+};
