e6b1674885142882f5c2de555ef82aeeb403543a
Forge-site1 <work.temoe@gmail.com>
2026-06-11 06:42:12 +0200

    Update addToBrevo.js

--- netlify/functions/addToBrevo.js
+++ netlify/functions/addToBrevo.js
@@ -1,46 +1,31 @@
 exports.handler = async (event) => {
-  console.log(
-  "KEY PREFIX:",
-  process.env.BREVO_API_KEY?.substring(0, 8)
-);
   try {
     const { email } = JSON.parse(event.body);
 
-    const response = await fetch(
-      'https://api.brevo.com/v3/contacts',
-      {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'api-key': process.env.BREVO_API_KEY
-        },
-        body: JSON.stringify({
-          email,
-          listIds: [4],
-          updateEnabled: true
-        })
-      }
-    );
+    const response = await fetch('https://api.brevo.com/v3/contacts', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'api-key': process.env.BREVO_API_KEY
+      },
+      body: JSON.stringify({
+        email,
+        listIds: [4],
+        updateEnabled: true
+      })
+    });
 
     const data = await response.text();
-
-    console.log("STATUS:", response.status);
-    console.log("RESPONSE:", data);
 
     return {
       statusCode: response.status,
       body: data
     };
-
   } catch (error) {
-    console.log("ERROR:", error);
-
     return {
       statusCode: 500,
-      body: JSON.stringify({
-        error: error.message
-      })
+      body: JSON.stringify({ error: error.message })
     };
   }
 };
